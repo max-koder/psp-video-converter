5811f25495d2dcd4528344322bf59af43869346a
max-koder <jager_88@list.ru>
2026-03-01 10:25:19 +0200

    Added Rich in my code, now it looks a lot better.
    Next plans:
    1. Hide ffmpeg data in terminal.
    2. Find a way to track conversion progress.
    3. Add error handling.

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 from pathlib import Path
 from presets import PRESETS
-from basics import VideoPreset
+from rich.console import Console
+from rich.panel import Panel
+from rich.table import Table
 import ffmpeg
+
+console = Console()
 
 def get_input_output_data():
     inp_out_list = []
-    print('To start conversion, write path to file, which you want to convert')
+
+    console.print('Enter [red][bold]FULL[/red][/bold] path to your input video'),
+    console.print('[green]Example[/green]: /username/Videos/input_video_name.mp4'),
+    console.print('[bold]YOUR INPUT PATH[/bold]: ', end=""),
+
     inp_out_list.append(str(Path(input().strip())))
-    print('Thanks! Please, write path, where you want to find output file, with filename')
+    console.print('\n')
+
+    console.print('Enter [red][bold]FULL[/red][/bold] path where you want to get your output video'),
+    console.print('[green]Example[/green]: /username/Videos/output_video_name.mp4'),
+    console.print('[bold]YOUR OUTPUT PATH[/bold]: ', end=""),
+
     inp_out_list.append(str(Path(input().strip())))
+    console.print('\n')
+
     return inp_out_list
 
 def preset_choice():
 
-    print("Welcome to PSP Video Converter!"
-          "To start work with application, choose presets for video conversion\n")
+    console.print(Panel("[bold]Welcome to PSP Video Converter![/bold]\n\n"
+          "To start work with application, choose presets for video conversion.\n"))
+
+    table = Table(title = "Presets", padding = (1, 2), min_width=60)
+
+    table.add_column("ID", justify="left", style="cyan", no_wrap=True)
+    table.add_column("Preset", style="magenta")
+    table.add_column("Description", justify="left", style="green")
 
     for presets in PRESETS:
-        print(f'{presets.key}. {presets.name}')
-        print(f'{presets.description}\n')
+        table.add_row(f"{presets.key}", f"{presets.name}", f"{presets.description}")
 
-    print('Please, choose preset from 1-3: ')
+    console.print(table)
+    console.print("To choose preset write it [red][bold]ID[/red][/bold]: ", end="")
     preset_chosen = int(input())
+    console.print('\n')
 
     return preset_chosen
 
@@ -34,6 +56,7 @@ def make_conversion(preset_chosen ,inp_out_list):
     output_options = PRESETS[preset_chosen-1].convert_to_ffmpeg_settings()
 
     ffmpeg.input(input_path).output(output_path, **output_options).run()
+
     print("Conversion completed")
 
 make_conversion(preset_choice(), get_input_output_data())
